af914f248989b5743c956c9b4981ee63632404f4
Atlis-Developer <utts.kyiv@gmail.com>
2021-09-21 19:03:01 +0300

    update elemets

--- src/components/Examples/Examples.jsx
+++ src/components/Examples/Examples.jsx
@@ -1,9 +1,77 @@
-import React from 'react';
+import React, {useState, useEffect} from "react"
+import slide1 from '../../images/Motobox01.jpg'
+import slide2 from '../../images/Motobox02.jpg'
+import slide3 from '../../images/Motobox03.jpg'
+import slide4 from '../../images/Motobox04.jpg'
+import slide5 from '../../images/Motobox03.jpg'
+import './Examples.css';
+
 
 const Examples = () => {
-  return ( 
-    <div className="Examples">
-      
+  const img = [
+    <img key={slide1} src={slide1} />,
+    <img key={slide2} src={slide2} />,
+    <img key={slide3} src={slide3} />,
+    <img key={slide4} src={slide4} />,
+    <img key={slide5} src={slide5} />,
+]
+
+const [activeIndex, setActiveIndex] = useState(0);
+
+
+// Хук Effect
+useEffect(() => {
+    // Запускаем интервал
+    const interval = setInterval(() => {
+        // Меняем состояние
+        setActiveIndex((current) => {
+            // Вычисляем индекс следующего слайда, который должен вывестись
+            const res = current === img.length - 1 ? 0 : current + 1
+            // Возвращаем индекс
+            return res
+        })
+    }, 5000)
+    // Выключаем интервал
+    return () => clearInterval()
+}, activeIndex)
+ 
+// Вычисляем индекс предыдущего слайда
+const prevImgIndex = activeIndex ? activeIndex - 1 : img.length - 1
+// Вычисляем индекс следующего слайда
+const nextImgIndex = activeIndex === img.length - 1 ? 0 : activeIndex + 1
+
+
+const nextSlide = () =>{
+  setActiveIndex((current) => {
+    const res = current === img.length - 1 ? 0 : current + 1
+    return res
+})
+};
+
+const previousSlide = () =>{
+  setActiveIndex((current) => {
+    const res = current === 0 ? img.length -1 : current - 1
+    return res
+})
+};
+
+
+return ( 
+    <div className="slider">
+        <div className="slider-img slider-img-prev"
+                key={prevImgIndex}>
+            {img[prevImgIndex]}
+        </div>
+        <div className="slider-img"
+                key={activeIndex}>
+            {img[activeIndex]}
+        </div>
+        <div className="slider-img slider-img-next"
+                key={nextImgIndex}>
+            {img[nextImgIndex]}
+        </div>
+        <a class="previous" onClick={()=>previousSlide()}>&#10094;</a>
+        <a class="next" onClick={()=>nextSlide()}>&#10095;</a>
     </div>
   );
 }
--- src/components/Motoboxs/Motoboxs.jsx
+++ src/components/Motoboxs/Motoboxs.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import BigImg from '../../images/Motobox01.jpg'
+import ImgLeft from '../../images/Motobox02.jpg'
+import ImgCenter from '../../images/Motobox03.jpg'
+import ImgRight from '../../images/Motobox04.jpg'
+import './Motoboxs.css';
+
 
 const Motoboxs = () => {
-  return ( 
-    <div className="Motoboxs">
-      
+  return (
+    <div className="motoboxs">
+      <h1>МОТОБОКСЫ</h1>
+      <div className='txt'>
+        Данные мотобоксы обеспечивают безопасное хранение мотоциклов, надёжную защиту от любых воздействий окружающей среды, а специальная система крепежа позволяет удобно и беспрепятственно размещать мото технику внутри бокса.
+      </div>
+      <div className='big-img'>
+      <img src={BigImg} />
+      </div>
+      <div className='small-img'>
+        <div className='image'>
+          <img src={ImgLeft} />
+        </div>
+        <div className='image'>
+          <img src={ImgCenter} />
+        </div>
+        <div className='image'>
+          <img className='right' src={ImgRight} />
+        </div>
+      </div>
     </div>
   );
 }
