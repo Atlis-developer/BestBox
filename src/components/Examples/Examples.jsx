ff24620cbe90952dca38e8bd369a86def7f828a3
Atlis-Developer <utts.kyiv@gmail.com>
2021-09-21 19:05:01 +0300

    update

--- src/components/Examples/Examples.jsx
+++ src/components/Examples/Examples.jsx
@@ -19,25 +19,18 @@ const Examples = () => {
 const [activeIndex, setActiveIndex] = useState(0);
 
 
-// Хук Effect
+
 useEffect(() => {
-    // Запускаем интервал
     const interval = setInterval(() => {
-        // Меняем состояние
         setActiveIndex((current) => {
-            // Вычисляем индекс следующего слайда, который должен вывестись
             const res = current === img.length - 1 ? 0 : current + 1
-            // Возвращаем индекс
             return res
         })
     }, 5000)
-    // Выключаем интервал
     return () => clearInterval()
 }, activeIndex)
  
-// Вычисляем индекс предыдущего слайда
 const prevImgIndex = activeIndex ? activeIndex - 1 : img.length - 1
-// Вычисляем индекс следующего слайда
 const nextImgIndex = activeIndex === img.length - 1 ? 0 : activeIndex + 1
 
 
